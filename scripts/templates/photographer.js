fb3d394c013e62d50379e38762991860931eae0b
William <william.deazevedopro@gmail.com>
2024-05-03 16:56:47 +0200

    accessiblite gallery

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -19,19 +19,20 @@ function photographerTemplate(data) {
   function createGallery(medias) {
     const { title, image, video, likes } = medias;
     const pictureMedia = `assets/photographers/${name}/${image ? image : video}`;
-    const figureElement = createElementWithClass('figure', 'img-container', '');
+    const figureLink = createElementWithClass('a', 'img-container', '');
     const mediaImage = createImage(pictureMedia, title, video ? 'video' : 'image');
     const figcaption = createElementWithClass('figcaption', 'image-caption', '');
     const p = createElementWithClass('p', '', title);
     const likeContainer = createElementWithClass('div', 'like-container', '');
     const likeCount = createElementWithClass('p', 'like-count', likes);
-    const link = createElementWithClass('a', 'image-swiper');
-    figureElement.append(link, figcaption);
+    const link = createElementWithClass('figure', 'image-swiper');
+    figureLink.href = '#';
+    figureLink.append(link, figcaption);
     figcaption.append(p, likeContainer);
     likeContainer.append(likeCount);
     likeContainer.innerHTML += ` <svg aria-label="likes" class="heart toggle-like" width="19" height="19" viewBox="0 0 19 19" xmlns="http://www.w3.org/2000/svg"> <path d="M9.5 18.35L8.23125 17.03C3.725 12.36 0.75 9.28 0.75 5.5C0.75 2.42 2.8675 0 5.5625 0C7.085 0 8.54625 0.81 9.5 2.09C10.4537 0.81 11.915 0 13.4375 0C16.1325 0 18.25 2.42 18.25 5.5C18.25 9.28 15.275 12.36 10.7688 17.04L9.5 18.35Z"/> </svg>`;
     link.appendChild(mediaImage)
-    return { figureElement };
+    return { figureLink };
   }
 
   // Fonction pour ajouter le nom du photographe dans la boîte de dialogue de contact
--- scripts/utils/dropdown.js
+++ scripts/utils/dropdown.js
@@ -1,66 +1,77 @@
 // Sélectionne les éléments du DOM nécessaires
-const dropdown = document.querySelector('#sort-options')
-const dropdownContent = document.querySelector('.dropdown-content')
-const dropdownList = document.querySelectorAll('.dropdown-content > li')
-const dropdownCaret = document.querySelector('.caret')
-const selected = document.querySelector('.selected')
-let selectedValue = selected.textContent
+const dropdown = document.querySelector('#sort-options');
+const dropdownList = document.querySelectorAll('.dropdown-btn');
+const dropdownCaret = document.querySelector('.caret');
+const selected = document.querySelector('.selected');
+let selectedValue = selected.textContent;
 
 // Fonction pour initialiser le comportement du dropdown
 function initDropdown(data) {
   // Ajoute un écouteur d'événement au clic sur le bouton du dropdown
-  dropdown.addEventListener('click', function (e) {
+  dropdown.addEventListener('click', function(e) {
     // Bascule l'attribut aria-expanded pour ouvrir/fermer le dropdown
     if (dropdown.getAttribute('aria-expanded') === "false") {
-      dropdown.setAttribute('aria-expanded', "true")
+      dropdown.setAttribute('aria-expanded', "true");
     } else {
-      dropdown.setAttribute('aria-expanded', "false")
+      dropdown.setAttribute('aria-expanded', "false");
     }
-    // Ajoute/supprime la classe 'inactive' sur les options du dropdown
-    dropdownList.forEach(function (element) {
-      element.classList.toggle('inactive')
-    })
+
+    // Définit l'attribut aria-hidden sur les options du dropdown
+    dropdownList.forEach(function(element) {
+      if (element.getAttribute('aria-hidden') === "true") {
+        element.setAttribute('aria-hidden', "false");
+      } else {
+        element.setAttribute('aria-hidden', "true");
+      }
+    });
+
     // Fait pivoter la flèche du dropdown
-    dropdownCaret.classList.toggle('rotate')
-  })
+    dropdownCaret.classList.toggle('rotate');
+  });
 
   // Fonction pour fermer le dropdown
   function closeDropdown() {
     // Définit l'attribut aria-expanded sur "false"
-    dropdown.setAttribute('aria-expanded', "false")
-    // Ajoute la classe 'inactive' à toutes les options du dropdown
-    dropdownList.forEach(function (element) {
-      element.classList.add('inactive')
-    })
+    dropdown.setAttribute('aria-expanded', "false");
+
+    // Définit l'attribut aria-hidden sur "true" pour toutes les options du dropdown
+    dropdownList.forEach(function(element) {
+      element.setAttribute('aria-hidden', "true");
+    });
+
     // Supprime la rotation de la flèche du dropdown
-    dropdownCaret.classList.remove('rotate')
+    dropdownCaret.classList.remove('rotate');
   }
 
   // Ajoute un écouteur d'événement au clic sur les options du dropdown
-  dropdownList.forEach(function (element) {
-    element.addEventListener('click', function (e) {
-      e.preventDefault()
+  dropdownList.forEach(function(element) {
+    element.addEventListener('click', function(e) {
+      e.preventDefault();
+
       // Met à jour la valeur sélectionnée
-      selectedValue = e.target.textContent
-      e.target.textContent = selected.textContent
-      selected.textContent = selectedValue
+      selectedValue = e.target.textContent;
+      e.target.textContent = selected.textContent;
+      selected.textContent = selectedValue;
+
       // Ferme le dropdown
-      closeDropdown()
+      closeDropdown();
+
       // Détermine le critère de tri en fonction de la valeur sélectionnée
       let sortBy;
       switch (selectedValue) {
         case 'Popularité':
-          sortBy = 'likes'
+          sortBy = 'likes';
           break;
         case 'date':
-          sortBy = 'date'
+          sortBy = 'date';
           break;
         case 'Titre':
-          sortBy = 'title'
+          sortBy = 'title';
           break;
       }
+
       // Crée la galerie triée selon le critère choisi
-      createTheGallery(data, sortBy)
-    })
-  })
+      createTheGallery(data, sortBy);
+    });
+  });
 }
