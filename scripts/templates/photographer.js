274b446294f477c53eaedbdbf4f703f8d6608679
William <william.deazevedopro@gmail.com>
2024-05-30 14:19:26 +0200

    ajout coeur filled or outlined

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -31,9 +31,9 @@ function photographerTemplate(data) {
     figcaption.append(p, likeContainer);
     likeContainer.append(likeCount);
     likeContainer.innerHTML += `<a class="toggle-like" href="#" aria-label="Like">
-      <svg aria-hidden="true" class="heart" width="19" height="19" viewBox="0 0 19 19" xmlns="http://www.w3.org/2000/svg">
-      <path d="M9.5 18.35L8.23125 17.03C3.725 12.36 0.75 9.28 0.75 5.5C0.75 2.42 2.8675 0 5.5625 0C7.085 0 8.54625 0.81 9.5 2.09C10.4537 0.81 11.915 0 13.4375 0C16.1325 0 18.25 2.42 18.25 5.5C18.25 9.28 15.275 12.36 10.7688 17.04L9.5 18.35Z"/>
-      </svg>
+    <svg aria-hidden="true" class="heart-outlined" width="19" height="19" viewBox="0 0 19 19" xmlns="http://www.w3.org/2000/svg">
+      <path class="heart-path" d="M9.5 18.35L8.23125 17.03C3.725 12.36 0.75 9.28 0.75 5.5C0.75 2.42 2.8675 0 5.5625 0C7.085 0 8.54625 0.81 9.5 2.09C10.4537 0.81 11.915 0 13.4375 0C16.1325 0 18.25 2.42 18.25 5.5C18.25 9.28 15.275 12.36 10.7688 17.04L9.5 18.35Z"/>
+    </svg>
     </a>`;
     link.appendChild(mediaImage)
     return { figureElement };
--- scripts/utils/like.js
+++ scripts/utils/like.js
@@ -1,29 +1,34 @@
 function initLikes(likes) {
   // Stocke les éléments boutons "like" dans la variable likeBtn
   let likeBtn = likes;
-
+  
   likeBtn.forEach(button => {
     // Initialise la variable clicked à false pour chaque bouton
     let clicked = false;
-
+    
     // Ajoute un écouteur d'événement 'click' à chaque bouton
     button.addEventListener('click', function (e) {
       e.preventDefault();
+      
       // Trouve le conteneur le plus proche contenant le bouton et le compteur de likes
       const likeContainer = e.currentTarget.closest('.like-container');
       const closestLikeCount = likeContainer.querySelector('.like-count');
-
+      console.log(button);
+      button.classList.toggle('heart-filled');
       // Si le bouton a déjà été cliqué
       if (clicked) {
+        
         // Réinitialise clicked à false
         clicked = false;
         // Décrémente le compteur de likes
         closestLikeCount.textContent = parseInt(closestLikeCount.textContent) - 1;
         // Décrémente le total de likes
         totalLikesSpan.textContent = parseInt(totalLikesSpan.textContent) - 1;
-      } else { // Si le bouton n'a pas encore été cliqué
+      } else {
+        // Si le bouton n'a pas encore été cliqué
         // Définit clicked à true
         clicked = true;
+        
         // Incrémente le compteur de likes
         closestLikeCount.textContent = parseInt(closestLikeCount.textContent) + 1;
         // Incrémente le total de likes
